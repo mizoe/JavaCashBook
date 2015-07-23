e171bdd4d710bf6b27d989b638403934835e91b9
Tomo Mizoe <mz@july.co.jp>
2015-07-23 12:21:48 +0900

    不要なコードの整理、コメント追加

--- src/CashBook.java
+++ src/CashBook.java
@@ -9,23 +9,20 @@ public class CashBook {
 		 journal = new ArrayList<Journal>();
 	}
 
-	public int getLastAmount(Calendar cal){
+	// TODO: 最後の残高を知りたい（最低限の計算で更新できるようにしたいので）
+	public int getLastBalance(Calendar cal){
 		int lastAmount = 0;
-		//日付でソート
-		Collections.sort(journal, new JournalComparator());
-		//ソート確認
-		for(int i=0;i<journal.size();i++){
-			System.out.format(journal.get(i).toString());
-		}
 		return lastAmount;
 	}
 
-	public void sort(){ // 日付でソート
+	// 日付でソート
+	public void sort(){
 		Collections.sort(journal, new JournalComparator());
 		//残高更新
 		this.updateBalance();
 	}
 
+	//すべての仕訳の残高を更新 TODO:最低限の計算で更新できるようにする
 	public void updateBalance(){
 		int balance = 0;
 		for(int i=0;i<journal.size();i++){
@@ -33,12 +30,15 @@ public class CashBook {
 			journal.get(i).setBalance(balance);
 		}
 	}
+
+	//画面にすべての仕訳を出力
 	public void printAll(){
 		for(int i=0;i<journal.size();i++){
 			System.out.format(journal.get(i).toString() + "\n");
 		}
 	}
 
+	//仕訳の追加
 	public void add(int year, int month, int day, int hour, int minute, String usage, int amount){
 		journal.add(new Journal(year, month, day, hour, minute, usage, amount));
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import java.util.ArrayList;
-
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("--- お小遣い帳 ---");
 
 		CashBook cb = new CashBook();
-		ArrayList<Journal> journal = new ArrayList<Journal>();
 		cb.add(2015, 1, 1, 9, 0, "繰越", 1000);
 		cb.add(2015, 1, 1, 10, 3, "ビール", -300);
 		cb.add(2015, 1, 1, 10, 1, "かわ", -160);
